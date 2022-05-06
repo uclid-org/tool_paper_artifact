94ed091003a19e2bfb92230bc0c920f77aac91f9
Kevin Cheang <kkmc.dev@gmail.com>
2022-05-05 21:43:21 -0700

    Added boogie and updated z3 version
    The previous version of z3 got stuck solving the safety properties of
    TAP

--- uclid_artefact/tool_paper_examples/run_TrustedAbstractPlatform.sh
+++ uclid_artefact/tool_paper_examples/run_TrustedAbstractPlatform.sh
@@ -5,11 +5,11 @@ cd TrustedAbstractPlatform
 
 echo "Verifying TAP model ..."
 cd modules
-#time make tap
+time make tap
 
 echo "Verify integrity proof ..."
 cd ../proofs
-#time make
+time make
 
 echo "Returning to examples directory ..."
 cd ../../
